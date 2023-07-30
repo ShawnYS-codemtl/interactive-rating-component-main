81f2b24bb20bad00f4bd9b3976b0b48718c2eccb
ShawnYS-codemtl <79675591+ShawnYS-codemtl@users.noreply.github.com>
2023-07-30 00:59:54 -0400

    linked the script and implemented active state for buttons

---
+++ index.js
@@ -0,0 +1,13 @@
+const submit_btn = document.querySelector('#submit');
+
+
+let rating = 0;
+
+function set_rating() {
+    rating = innerHTML;
+};
+
+function submit_rating() {
+    window.location.href = "thankyou.html";
+};
+
